9090f8eff790636105461d658587812cabc85c1a
LeugiMH <miguelhenriquegs369@gmail.com>
2022-08-24 23:08:34 -0300

    teste1

--- Ficha-RPG/routes/web.php
+++ Ficha-RPG/routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
+});
+*/
+Route::get('/', function() {
+    return view('teste', ['teste' => 'olá']);
 });
